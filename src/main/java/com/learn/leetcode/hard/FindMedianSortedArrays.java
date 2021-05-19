190bc217e0d2ff640a4052be34c55251a60e3535
郁帅奇 <shuaiqi@kaixingame.cn>
2021-05-19 10:21:51 +0800

    FindMedianSortedArrays

---
+++ src/main/java/com/learn/leetcode/hard/FindMedianSortedArrays.java
@@ -0,0 +1,57 @@
+package com.learn.leetcode.hard;
+
+/**
+ * @description: findMedianSortedArrays
+ * @author: gary
+ * @create: 2021-05-18 19:00
+ * @version: 1.0
+ **/
+public class FindMedianSortedArrays {
+
+    public double findMedianSortedArrays(int[] num1, int[] num2) {
+        int m = num1.length;
+        int n = num2.length;
+        int[] nums = new int[m + n];
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (num2[n / 2 - 1] + num2[n / 2]) / 2.0;
+            } else {
+                return num2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (num1[m / 2 - 1] + num1[m / 2]) / 2.0;
+            } else {
+                return num1[m / 2];
+            }
+        }
+        int count = 0;
+        int i = 0, j = 0;
+        while (count != (m + n)) {
+            if (i == m) {
+                while (j != n) {
+                    nums[count++] = num2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i != m) {
+                    nums[count++] = num1[i++];
+                }
+                break;
+            }
+            if (num1[i] < num2[j]) {
+                nums[count++] = num1[i++];
+            } else {
+                nums[count++] = num2[j++];
+            }
+        }
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count /2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
+    }
+
+}
